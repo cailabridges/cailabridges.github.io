104682ce5998808dd0e0af7d18d7a6aede85907d
Caila Bridges <146654762+cailabridges@users.noreply.github.com>
2023-10-02 08:41:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,28 @@ jQuery(document).ready(function($) {
 		  	nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='ri-arrow-right-line'></i></button>"
 		});
     }
-   
+     // Array of image URLs
+const testimonialImages = [
+  'projects-1.png',
+  'nn1.png',
+  'projects-2.png',
+  'practices1.png',
+  'process-2.png',
+  // Add more image URLs as needed
+];
+
+// Get the testimonials-slider element
+const testimonialsSlider = document.getElementById('testimonials-slider');
+
+// Loop through the image URLs and create image elements
+testimonialImages.forEach(imageUrl => {
+  // Create an img element
+  const imageElement = document.createElement('img');
+  imageElement.src = imageUrl;
+  imageElement.alt = 'Testimonial Image';
+
+  // Append the image to the testimonials-slider
+  testimonialsSlider.appendChild(imageElement);
+});
 
 });
